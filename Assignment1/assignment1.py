de4dc5b2e45c0aeb832f8d17d6a5a907bc59fc2f
cbasil98 <cbasil98@gmail.com>
2018-09-07 00:27:00 +0530

    1.Modification1
    Includes Sort
    Slight modification in other parts

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,15 +1,20 @@
 names=list()
 a='y'
 while a=='y':
-    x=int(input('Select an option:\n1. Insert\n2. Remove\n3. Print all\n4.Remove all\n'))
+    x=int(input('Select an option:\n1. Insert\n2. Remove\n3. Print all\n4.Sort 5.Remove all\n'))
     if x==1:
-        name=input('Enter the name')
-        names.append(name)
+        n=int(input(Enter the number of entries))
+        for i in range(n):
+            name=input('Enter the name')
+            names.append(name)
     elif x==2:
         name=input('Enter the name to remove')
         names.remove(name)
     elif x==3:
-        print(names)
+        for i in range(len(names)):
+            print(i+1,'.',names(i))
+    elif x==4:
+        names.sort()
     elif x==4:
         del names[:]
     else:
