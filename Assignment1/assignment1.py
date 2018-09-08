99129839eee20b2f2805ec5f39825aab1e15e419
cbasil98 <cbasil98@gmail.com>
2018-09-08 17:46:48 +0530

    Improved readability
    minor bug fixes

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,21 +1,27 @@
 names=list()
 a='y'
 while a=='y':
-    x=int(input('Select an option:\n1. Insert\n2. Remove\n3. Print all\n4.Sort 5.Remove all\n'))
+    print("Select an option")
+    print("1.Insert")
+    print('2.Remove')
+    print('3.Print all')
+    print('4.Sort')
+    print('5.Remove all')
+    x=int(input())
     if x==1:
-        n=int(input(Enter the number of entries))
+        n=int(input('Enter the number of entries:'))
         for i in range(n):
-            name=input('Enter the name')
+            name=input('Enter the name:')
             names.append(name)
     elif x==2:
-        name=input('Enter the name to remove')
+        name=input('Enter the name to remove:')
         names.remove(name)
     elif x==3:
         for i in range(len(names)):
-            print(i+1,'.',names(i))
+            print(i+1,'.',names[i])
     elif x==4:
         names.sort()
-    elif x==4:
+    elif x==5:
         del names[:]
     else:
         print('Invalid entry')
