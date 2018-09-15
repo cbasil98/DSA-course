1c15d86b39066dbbc5e21004a553a2b86bf50b22
cbasil98 <cbasil98@gmail.com>
2018-09-15 23:14:33 +0530

    More functions
    added search

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,11 +1,12 @@
+import os
 names=list()
-a='y'
-while a=='y':
+while 1:
     print("Select an option")
     print("1.Insert")
     print('2.Remove')
     print('3.Print all')
     print('4.Sort')
+    print('5.Search')
     print('5.Remove all')
     x=int(input())
     if x==1:
@@ -21,9 +22,24 @@ while a=='y':
             print(i+1,'.',names[i])
     elif x==4:
         names.sort()
+        print("List sorted")
     elif x==5:
+        name=input("Enter the number to search:")
+        for i in range(len(names)):
+            flag = 0
+            if name==names[i]:
+                print("Saved as ",i+1,"th entry")
+                flag=1
+                break
+        if flag==0:
+            print("Not found")
+    elif x==6:
         del names[:]
     else:
         print('Invalid entry')
     a=input('Do you want to continue?(y/press any other key)')
+    if a!='y':
+        break;
+    os.system('cls')
+
 
