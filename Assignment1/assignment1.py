1d197180facb5728b333792190df11f24f361d79
cbasil98 <cbasil98@gmail.com>
2018-09-16 22:43:09 +0530

    More functions
    print statement to every case

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,4 +1,3 @@
-import os
 names=list()
 while 1:
     print("Select an option")
@@ -7,16 +6,29 @@ while 1:
     print('3.Print all')
     print('4.Sort')
     print('5.Search')
-    print('5.Remove all')
-    x=int(input())
+    print("6.Remove all")
+    print("7.Exit")
+    x=input()
+    if x.isnumeric()==False:
+        print("Invalid entry")
+        continue
+    x=int(x)
+
     if x==1:
-        n=int(input('Enter the number of entries:'))
+        while 1:
+            n=input('Enter the number of entries:')
+            if n.isnumeric()==False:
+                print("Invalid Entry")
+                continue
+            break
         for i in range(n):
             name=input('Enter the name:')
             names.append(name)
+        print("All entries are added")
     elif x==2:
         name=input('Enter the name to remove:')
         names.remove(name)
+        print("Item removed")
     elif x==3:
         for i in range(len(names)):
             print(i+1,'.',names[i])
@@ -28,18 +40,18 @@ while 1:
         for i in range(len(names)):
             flag = 0
             if name==names[i]:
-                print("Saved as ",i+1,"th entry")
+                print("Saved as",i+1,"th entry")
                 flag=1
                 break
         if flag==0:
             print("Not found")
     elif x==6:
         del names[:]
+        print("All data is deleted")
+    elif x==7:
+        break
     else:
         print('Invalid entry')
     a=input('Do you want to continue?(y/press any other key)')
     if a!='y':
         break;
-    os.system('cls')
-
-
